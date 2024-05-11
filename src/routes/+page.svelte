746499ee9634d7a1f980666923f74c2c7bfa9e9a
Huda Mubarak <huda.mubarak@student.uhasselt.be>
2024-05-11 17:33:23 +0200

    usage of sollar sign only ok

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,18 @@
 <script>
   import { createEventDispatcher } from 'svelte';
+  import { writable } from 'svelte/store';
   import Graph1 from '$lib/Graph1.svelte'; // Import the Graph1 component
 
   const dispatch = createEventDispatcher(); // Event dispatcher to communicate with parent components
 
-  let selectedFileName = ''; // Initialize the selected file name variable
-
-  // Function to handle button click and set the selected file name
-  function selectFileName(fileName) {
-    selectedFileName = fileName;
-    dispatch('fileSelected', { fileName }); // Dispatch event to communicate with parent components
-  }
+  // Initialize the selected file name as a writable store
+  $: selectedFileName = writable('');
+  $: fileName = "Forcast_ActualSales2024";
+// Function to handle button click and set the selected file name
+function selectFileName($fileName) {
+  selectedFileName.set($fileName); // Set the value of the writable store
+  dispatch('fileSelected', { $fileName }); // Dispatch event to communicate with parent components
+}
 </script>
 
 <div>
@@ -21,8 +23,8 @@
   <!-- Button to select "Forcast_ActualSales2024" file name -->
   <button on:click={() => selectFileName("Forcast_ActualSales2024")}>Load 2024 Data</button>
 
-  <!-- Render Graph1 component with the selected file name -->
-  {#if selectedFileName}
-    <Graph1 fileName={selectedFileName} />
+  <!-- Render Graph1 component with the selected file name if available -->
+  {#if $selectedFileName}
+    <Graph1 fileName={$selectedFileName} />
   {/if}
 </div>
